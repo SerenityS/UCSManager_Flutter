85b27701bd86ef9d43dab142b4782541867ac143
HeeRyun, Jin <jins4218@gmail.com>
2021-02-21 23:43:58 +0900

    UCS List 마진 삭제

--- lib/screens/UCSListScreen.dart
+++ lib/screens/UCSListScreen.dart
@@ -64,7 +64,6 @@ class _UCSListScreen extends State<UCSListScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
       child: Center(
         child: FutureBuilder(
           future: getUCSList(),
